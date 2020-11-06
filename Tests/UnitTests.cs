53f8c03b1fd81bba2e3326d4a858f05181b5c76a
Chase Hermreck <chermreck2@huskers.unl.edu>
2020-11-05 19:38:30 -0600

    fixed test Subtract_Valid()

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -33,7 +33,7 @@ namespace TravisCILab
         [Test]
         public void Subtract_Valid()
         {
-            Assert.AreEqual(9, Program.Subtract("1", "2"));
+            Assert.AreEqual(-1, Program.Subtract("1", "2"));
             Assert.AreEqual(1, Program.Subtract("3", "2"));
             Assert.AreEqual(-2, Program.Subtract("5", "7"));
         }
